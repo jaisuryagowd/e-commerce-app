45efc9158f9daf7c1e8e0e436d35e70b201150d6
jaisuryagowd <jaisuryagowd25@gmail.com>
2025-09-21 16:17:40 +0530

    ecommerce start project

---
+++ services/config-server/src/test/java/com/jai/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jai.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
